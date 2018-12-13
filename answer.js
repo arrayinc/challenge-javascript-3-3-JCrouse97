991cd493d44fb1c7e8b23a33909e68b19c32fcaa
JCrouse97 <jcrouse@thearrayschool.com>
2018-12-13 15:04:59 -0700

    the numbers have been divided

--- answer.js
+++ answer.js
@@ -1,1 +1,15 @@
 // *** YOUR ANSWER BELOW ***
+
+function divBySixOrSeven(x){
+  let str = "";
+  for(let i = x; i > 0; i--){
+    if(i % 7 === 0){
+      str += `${i}, `;
+    } else if(i % 6 === 0){
+      str += `${i}, `;
+    }
+  }
+  return str;
+}
+
+divBySixOrSeven(100);
